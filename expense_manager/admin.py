9e4d7a5ef6ee8a660e34053ea10d1ce1a6936a41
Preetam <android.preetam@gmail.com>
2018-07-13 01:34:46 +0530

    Image filter added in admin to check null and empty values

--- expense_manager/admin.py
+++ expense_manager/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.db.models import Q
+from django.utils.html import mark_safe
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+# core django
+from .models import Expense
+
+
+from django.contrib import admin
+
+
+class ImageFilter(admin.SimpleListFilter):
+    title = u'Image'
+
+    parameter_name = u'photo'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'has image', ),
+            ('0', 'no image', ),
+        )
+
+    def queryset(self, request, queryset):
+        check_null = {
+            '%s' % self.parameter_name: None,
+        }
+        check_blank = {
+            '%s' % self.parameter_name: '',
+        }
+        if self.value() == '1':
+            return queryset.exclude(Q(**check_null) | Q(**check_blank))
+        if self.value() == '0':
+            return queryset.filter(Q(**check_null) | Q(**check_blank))
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created', 'modified')
+    list_filter = ('created', 'modified', ImageFilter)
+    search_fields = ('name', 'price')
+    ordering = ('-modified', '-created')
+
+    def photo(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.headshot.url,
+            width=obj.headshot.width,
+            height=obj.headshot.height,))
