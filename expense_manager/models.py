669485d619a971f3e3c6046d002fceb826c161d3
Preetam <android.preetam@gmail.com>
2018-07-13 01:35:30 +0530

    expense model changes and migrations

--- expense_manager/models.py
+++ expense_manager/models.py
@@ -1,3 +1,21 @@
+# core django
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Timestamp model to add created and modified field
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# Expense Model
+class Expense(TimeStampedModel):
+    user = models.ForeignKey(User, related_name='expenses',
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    photo = models.ImageField(upload_to='expense', null=True, blank=True)
