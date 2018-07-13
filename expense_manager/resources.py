266150a04c2aff32201c3d5df450c81a90da0a66
Preetam <android.preetam@gmail.com>
2018-07-14 01:13:08 +0530

    add expense user id auto added

--- expense_manager/resources.py
+++ expense_manager/resources.py
@@ -8,22 +8,42 @@ from .models import Expense
 from tastypie.resources import ModelResource
 from tastypie.constants import ALL
 from tastypie.authorization import DjangoAuthorization
+from tastypie import fields
 
-# tastypie task resource
+# tastypie user authorization
 
 
-class UserAuthorization(DjangoAuthorization):
-    def read_list(self, object_list, bundle):
-        return object_list.filter(user=bundle.request.user)
+# class UserAuthorization(DjangoAuthorization):
+#     def read_list(self, object_list, bundle):
+#         return object_list.filter(user=bundle.request.user)
+
+#     def update_list(self, object_list, bundle):
+#         allowed = []
+#         print(object_list)
+#         # Since they may not all be saved, iterate over them.
+#         for obj in object_list:
+#             if obj.user == bundle.request.user:
+#                 allowed.append(obj)
+
+#         return allowed
 
 
 class ExpenseResource(ModelResource):
+    # photo = fields.FileField(attribute="photo", null=True, blank=True)
     class Meta:
         queryset = Expense.objects.all()
         resource_name = 'expense'
-        authorization = UserAuthorization()
+        authorization = DjangoAuthorization()
         filtering = {
             'created': ['exact', 'range', 'gte'],
             'price': ALL
         }
-        excludes = ('modified')
+
+        excludes = ('id', 'created', 'modified')
+
+    def get_object_list(self, request):
+        return super(ExpenseResource, self).get_object_list(request).filter(user=request.user)
+
+    def hydrate(self, bundle):
+        bundle.obj.user = bundle.request.user
+        return bundle
