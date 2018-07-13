901dcc9a3e4c3415d12857edd1a5710dbc05f66c
Preetam <android.preetam@gmail.com>
2018-07-13 07:13:23 +0530

    expense media ignored and dashboard removed from accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,14 @@
 # core django
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordChangeView, PasswordChangeDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 # project
-from .views import register, dashboard
+from .views import register
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(
         template_name='accounts/login.html',
         redirect_authenticated_user=True), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('accounts:login')), name='logout'),
     path('register/', register, name='register'),
-    path('dashboard/', dashboard, name='dashboard')
 ]
