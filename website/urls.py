83c5bcb6b6207a9e34320b42bb6a73697c4da445
Preetam <android.preetam@gmail.com>
2018-07-13 07:11:16 +0530

    namespace accounts and expense_manager

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include(('expense_manager.urls','expense_manager'),namespace='expense_manager')),
+    path('', include(('accounts.urls','accounts'),namespace='accounts')),
+    path('', include(('expense_manager.urls', 'expense_manager'),
+                     namespace='expense_manager')),
+    re_path(r'^$', lambda r: HttpResponseRedirect(reverse('login'))),
 ]
