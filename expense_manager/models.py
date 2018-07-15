ad08df2be31d94b8a001fa45e0470ece8d7175fd
Preetam <android.preetam@gmail.com>
2018-07-15 20:54:38 +0530

    relative path removed imagefield

--- expense_manager/models.py
+++ expense_manager/models.py
@@ -69,7 +69,7 @@ class Expense(TimeStampedModel):
                              on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    photo = models.ImageField(upload_to='media/', null=True, blank=True)
+    photo = models.ImageField(upload_to='', null=True, blank=True)
     objects = models.Manager()
     manager = UserExpensesManager()
 
--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.shortcuts import reverse
 from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('accounts.urls','accounts'),namespace='accounts')),
+    path('', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('', include(('expense_manager.urls', 'expense_manager'),
                      namespace='expense_manager')),
     re_path(r'^$', lambda r: HttpResponseRedirect(reverse('accounts:login'))),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
