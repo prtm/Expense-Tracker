b77e3dd2d6a1476e4228c0576fe66613c1efb773
Preetam <android.preetam@gmail.com>
2018-07-15 19:42:35 +0530

    pagination with filter pages bug fixed

--- expense_manager/views.py
+++ expense_manager/views.py
@@ -28,6 +28,8 @@ def dashboard(request):
     budget_id = -1
     imagefilter = 1
     datefilter = 1
+    previous_page = None
+    next_page = None
 
     # add and updNoneate expense logic
     if request.method == "POST":
@@ -63,33 +65,57 @@ def dashboard(request):
         param_date = request.GET.get('date')
         param_offset = request.GET.get('offset')
         param_orderby = request.GET.get('order_by')
-        if param_name or param_name == '' or param_price or param_price == '':
+        if param_name or param_name == '':
             show_expense_tab = True
+            previous_page = "name="+param_name
+
+        if param_price or param_price == '':
+            show_expense_tab = True
+            if previous_page:
+                previous_page = "&price="+param_price
+            else:
+                previous_page = "price="+param_price
 
         # show expense tab if parameter found
         # change image filter text if image filter applied on page refresh
         if param_no_image or param_no_image == '':
             show_expense_tab = True
             imagefilter = 2
+            if previous_page:
+                previous_page = "&photo="+param_no_image
+            else:
+                previous_page = "photo="+param_no_image
 
         if param_has_image or param_has_image == '':
             show_expense_tab = True
             imagefilter = 3
+            if previous_page:
+                previous_page = "&photo__ne="+param_has_image
+            else:
+                previous_page = "photo__ne="+param_has_image
 
         if param_created:
             show_expense_tab = True
+            if previous_page:
+                previous_page = "&created__range="+param_created
+            else:
+                previous_page = "created__range="+param_created
             if param_date:
                 datefilter = param_date
 
         if param_offset:
             show_expense_tab = True
-        
+
         if param_orderby:
             show_expense_tab = True
+            if previous_page:
+                previous_page = "&order_by="+param_orderby
+            else:
+                previous_page = "order_by="+param_orderby
 
     # report summary
     total_expense = Expense.manager.total_expense(
-        request.user, date.today().month,date.today().year)['total_expense']
+        request.user, date.today().month, date.today().year)['total_expense']
     budget = Budget.objects.filter(
         user=request.user, month=date.today().month)
     if budget:
@@ -121,17 +147,31 @@ def dashboard(request):
     has_previous = (page_number != 1)
     has_next = (page_number != total_page_number)
 
+
+    # next page & previous page url 
+    next_page = previous_page
+    if previous_page:
+        previous_page += "&offset="+(str(int(offset)-20) if (int(offset)-20) > 0 else "0")
+        next_page += "&offset="+str(int(offset)+20)
+    else:
+        previous_page = "offset="+(str(int(offset)-20) if (int(offset)-20) > 0 else "0")
+        next_page = "offset="+str(int(offset)+20)
+
+    previous_page = "?"+previous_page
+    next_page = "?"+next_page
+
     return render(request, 'expense_manager/dashboard/dashboard.html', {'form': form, 'top_10': top_10,
                                                                         'all_expenses': all_expenses.get('objects'),
                                                                         'total_expense': total_expense,
                                                                         'budget': budget,
                                                                         'budget_id': budget_id,
-                                                                        'month' : date.today().strftime("%B"),
+                                                                        'month': date.today().strftime("%B"),
                                                                         'remaining': budget-total_expense,
                                                                         'show_expense_tab': show_expense_tab,
                                                                         'imagefilter': imagefilter,
                                                                         "datefilter": datefilter,
-                                                                        "offset": offset,
+                                                                        "previous_page": previous_page,
+                                                                        "next_page": next_page,
                                                                         "has_previous": has_previous,
                                                                         "has_next": has_next,
                                                                         "total_page_number": total_page_number,
